39bc9a814468375288afe001eeea461ef55ff922
Blueberry <114563745+WalterCheung-uwu@users.noreply.github.com>
2023-03-22 15:28:50 +0800

    Add files via upload

---
+++ Practice_AHHHHHHHH.java
@@ -0,0 +1,36 @@
+public class Practice_AHHHHHHHH
+{
+	public static void main (String[] args){
+			int num = IBIO.inputInt("enter number of lines: "); 
+			String aa = stars(num);
+			
+			for(int i = 0; i < num; i++){
+				System.out.println(stars(i + 1));
+		}
+			for(int i = num; i > 1; i--){
+				System.out.println(stars(i - 1));
+		}
+	}
+	
+	static String stars(int n){
+			String xx = "";
+			for(int i = 0; i < n; i++)
+				xx = xx + "*";
+			return xx;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
